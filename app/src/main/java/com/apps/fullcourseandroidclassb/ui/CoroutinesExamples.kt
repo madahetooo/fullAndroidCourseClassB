fadc66fb342182f6edd7e1b42cb1cf8d3f75602d
madahetooo <ieslammedhat@gmail.com>
2022-02-24 19:58:01 +0200

    Adding runBlocking, Await,Async,Join,Cancel and more..

--- app/src/main/java/com/apps/fullcourseandroidclassb/ui/CoroutinesExamples.kt
+++ app/src/main/java/com/apps/fullcourseandroidclassb/ui/CoroutinesExamples.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.apps.fullcourseandroidclassb.R
-import kotlinx.android.synthetic.main.activity_coroutines_examples.*
 import kotlinx.coroutines.*
 
 class CoroutinesExamples : AppCompatActivity() {
@@ -13,20 +12,49 @@ class CoroutinesExamples : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutines_examples)
-        GlobalScope.launch(Dispatchers.IO) {
-            //COROUTINE THREAD
-            val updatedText = doNetworkCall() //
-            withContext(Dispatchers.Main){
-                //I CAN ONLY UPDATE UI IN MAIN THREAD ONLYYYYYYYYY
-                tvDummy.text = updatedText
-            }
+
+        GlobalScope.launch(Dispatchers.Main) {
+           val answer1 = async {
+               doNetworkCall1()
+           }
+           val answer2 = async { doNetworkCall2() }
+            Log.d(TAG, "Answer 1 is equal to ${answer1.await()} ")
+            Log.d(TAG, "Answer 2 is equal to ${answer2.await()} ")
         }
-        //MAIN THREAD
-        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
+        runBlocking {
+            delay(423L)
+        }
+
     }
 
-    suspend fun doNetworkCall(): String {
-        delay(5000)
-        return "This is new text"
+    suspend fun doNetworkCall1(): String {
+        delay(8000L)
+        return "ANSWER 1"
+    }
+    suspend fun doNetworkCall2(): String {
+        delay(8000L)
+        return "ANSWER 2"
     }
 }
+
+
+
+
+
+
+
+
+
+
+//var answer1: String? = null
+//var answer2: String? = null
+//val job1 = launch {
+//    answer1 = doNetworkCall1()
+//}
+//val job2 = launch {
+//    answer2 = doNetworkCall2()
+//}
+//job1.join()
+//job2.join()
+//Log.d(TAG, "Answer 1 is equal to $answer1 ")
+//Log.d(TAG, "Answer 2 is equal to $answer2 ")
