b45b487d04636d72f827ff2503bea5aae881893a
madahetooo <ieslammedhat@gmail.com>
2022-03-13 21:54:48 +0200

    Adding Push Notification Firebase service

--- app/src/main/java/com/apps/fullcourseandroidclassb/data/local/db/NotificationAPI.kt
+++ app/src/main/java/com/apps/fullcourseandroidclassb/data/local/db/NotificationAPI.kt
@@ -3,12 +3,15 @@ package com.apps.fullcourseandroidclassb.data.local.db
 import com.apps.fullcourseandroidclassb.Constants
 import com.squareup.okhttp.ResponseBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface NotificationAPI {
 
-    @Headers("Authorization: key=${Constants.SERVER_KEY}","Content-Type:${Constants.CONTENT_TYPE}")
+    @Headers("Authorization: key=${Constants.SERVER_KEY}", "Content-Type:${Constants.CONTENT_TYPE}")
     @POST("/fcm/send")
-    suspend fun postNotification(notification: PushNotification):Response<ResponseBody>
+    suspend fun postNotification(
+        @Body notification: PushNotification
+    ): Response<ResponseBody>
 }
